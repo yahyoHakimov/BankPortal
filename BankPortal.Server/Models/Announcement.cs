9a362a09fbd49728e4a9f1da6edf3e50dd1f4ec6
hakim <yahyohakimov7@gmail.com>
2024-08-20 20:59:48 +0500

    Add initial models for Employee, Department, ExternalMeetings, Announcement, and Requests models

---
+++ BankPortal.Server/Models/Announcement.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankPortal.Server.Models
+{
+    public class Announcement
+    {
+        [Key]
+        public int AnnouncementId { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        public Employee Author { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime AnnouncementDate { get; set; }
+    }
+
+}
