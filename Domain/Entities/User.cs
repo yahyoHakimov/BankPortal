7db5f65cb644282aad2e33fe7dd86d292ee83256
hakim <yahyohakimov7@gmail.com>
2024-08-22 19:19:12 +0500

    Update: make update to system architecture,  add User table.

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    namespace BankPortal.Domain.Entities
+    {
+        public class User
+        {
+            public int UserID { get; set; }
+            public string Email { get; set; }
+            public string PasswordHash { get; set; }
+            public string PhoneNumber { get; set; }
+            public bool TwoFactorEnabled { get; set; }
+            public int RoleID { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
+
+            // Navigation Property
+            public virtual Employee Employee { get; set; }
+            public virtual ICollection<UserRole> UserRoles { get; set; }
+        }
+    }
+
+}
