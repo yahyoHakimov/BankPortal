21ffe1b850a00e29d531772921d50bf3b48654bf
hakim <yahyohakimov7@gmail.com>
2024-08-22 19:22:28 +0500

    Update: Add Department class

---
+++ Domain/Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+        public int ManagerID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Navigation Property
+        public virtual Employee Manager { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
