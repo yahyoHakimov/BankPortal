4df7093548bb4cb28bdafe4f824eac69d0d1f841
hakim <yahyohakimov7@gmail.com>
2024-08-22 19:19:49 +0500

    Update: Add Role entity

---
+++ Domain/Entities/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    namespace BankPortal.Domain.Entities
+    {
+        public class Role
+        {
+            public int RoleID { get; set; }
+            public string RoleName { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
+
+            // Navigation Property
+            public virtual ICollection<UserRole> UserRoles { get; set; }
+        }
+    }
+
+}
