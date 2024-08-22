12eac053c4c2433b1186bbb999ec17eb1ce00137
hakim <yahyohakimov7@gmail.com>
2024-08-22 19:23:24 +0500

    Update: Add Request class

---
+++ Domain/Entities/Request.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Request
+    {
+        public int RequestID { get; set; }
+        public int EmployeeID { get; set; }
+        public string RequestType { get; set; }
+        public string Status { get; set; }
+        public DateTime SubmittedAt { get; set; }
+        public DateTime? ApprovedAt { get; set; }
+        public string Comments { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Navigation Property
+        public virtual Employee Employee { get; set; }
+    }
+}
